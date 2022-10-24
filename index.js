b244ad86de693d67074ce7fbaab274b8500a39b7
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2022-10-24 13:46:26 -0400

    creating an array of chest exercises

---
+++ index.js
@@ -0,0 +1,22 @@
+let chestExercises = [{
+    name: "Bench press", 
+    muscles: "pectoralis major, deltoids, triceps",
+    image: "https://thumbs.gfycat.com/AngelicWhirlwindKronosaurus.webp"
+    },
+    {name: "Dumbbell bench press",
+    muscles: "pectoralis major, deltoids, triceps",
+    image: "https://thumbs.gfycat.com/DiscreteDismalApisdorsatalaboriosa.webp"
+    },
+    {name: "Machine bench press",
+    muscles: "pectoralis major, deltoids, triceps",
+    image: "https://thumbs.gfycat.com/LonelyVibrantKitfox-max-1mb.gif"
+    },
+    {name: "Incline dumbbell press",
+    muscles: "pectoralis major, deltoids, triceps",
+    image: "https://thumbs.gfycat.com/AmpleCooperativeIndigobunting.webp"
+    },
+    {name: "",
+    muscles: "",
+    image: ""
+    },
+]
