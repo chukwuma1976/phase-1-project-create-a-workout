16b42857a230c20c03ad095075c829651559bbea
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2022-10-27 19:11:04 -0400

    deleted junk code

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
-const exercises = {
-    chest: [],
-    shoulders: [],
-    back: [],
-    legs: [],
-    biceps: [],
-    triceps: [],
-    abdominals: [],
-    cardio: []
-}
-const workOutSplits = {
-    fullBody: {}, 
-    upperLower: {}, 
-    pushPullLegs: {}, 
-    arnoldSplit: {}, 
-    broSplit: {},
-    customSplit: {}
-}
+const exerciseKeys = ['chest', 'shoulders', 'back', 'legs', 'biceps', 'triceps', 'abdominals', 'cardio']
 
-const exerciseKeys = Object.keys(exercises)
-for (let keys of exerciseKeys){
-    fetch(`http://localhost:3000/${keys}`)
-    .then(response => response.json())
-    .then(data=>{
-        let i=0
-        for (let piece of data){
-            exercises[keys][i]=renderObject(piece)
-            i++
-        }
-        return exercises[keys]
-        })
-    .catch((error)=>alert("There is an error"))
-    }   
+const workOutSplits = {
+    fullBody: {
+        name: "Full body workout",
+        purpose: "works every muscle group in one workout",
+        musclesTrained: "chest, back, shoulders, arms and legs in one workout",
+        frequency: "2-6 times per week for a total of 2-6 days of training",
+        maxFrequency: 6
+    }, 
+    upperLower: {
+        name: "Upper lower workout",
+        purpose: "works the body in two workouts, one for upper body muscles and one for lower body muscles",
+        musclesTrained: "chest, back, shoulders, arms on one day and legs on the other day",
+        frequency: "2-3 times per week for a total of 4-6 days of training",
+        maxFrequency: 3
+    }, 
+    pushPullLegs: {
+        name: "Push pull legs workout",
+        purpose: "works the body in three workouts, one for push muscles, one for pull muscles, and one for lower body muscles",
+        musclesTrained: "chest, triceps, shoulders on day 1, back, shoulders, biceps on day 2, and legs on day 3",
+        frequency: "2 times per week for a total of 6 days of training",
+        maxFrequency: 2
+    }, 
+    arnoldSplit: {
+        name: "Arnold split workout, named after Arnold Schwarzenegger who used this split",
+        purpose: "works the body in three workouts, one for chest and back, one for shoulders and arms, and one for legs",
+        musclesTrained: "chest, triceps, shoulders on day 1, back, shoulders, biceps on day 2, and legs on day 3",
+        frequency: "2 times per week for a total of 6 days of training",
+        maxFrequency: 2
+    }, 
+    broSplit: {
+        name: "body part split, aka 'Bro' split popularized by gym bros around the world",
+        purpose: "works the body dedicating a day for each body part: ex. chest day, back day, shoulder day, arm day and leg day",
+        musclesTrained: "chest, back, shoulders, arms, legs each trained one day per week",
+        frequency: "once per week for a total of 5 days of training",
+        maxFrequency: 1
+    },
+    customSplit: {name: "a custom training split",
+        purpose: "a customized split that does not fit the other training splits",
+        musclesTrained: "whatever you feel like training",
+        frequency: "can vary",
+        maxFrequency: 7}
+}
 
 const splitKeys = Object.keys(workOutSplits)
-for (let keys of splitKeys){
-    fetch(`http://localhost:3000/${keys}`)
-    .then(response => response.json())
-    .then(data=>{
-            workOutSplits[keys]=renderObject(data)
-            return workOutSplits[keys]
-        })
-    .catch((error)=>alert("There is an error"))
-    }
-
-function renderObject(type){
-    let newObject = {}
-    let keys = Object.keys(type)
-    for (let key of keys){newObject[key]=type[key]}
-    return newObject
-}
 
 const legDay = {legs: []}
 const chestDay = {chest: []}
@@ -83,6 +78,7 @@ const pullDay = {
 }
 
 const chestAndBack = {chest: [], back: []}
+
 const shouldersAndArms = { 
     shoulders: [], 
     biceps: [], 
@@ -100,11 +96,6 @@ const arrayOfExerciseDays = [
     pullDay, 
     chestAndBack, 
     shouldersAndArms]
-
-// for (const day of arrayOfExerciseDays){
-//     for (let workout in day) {downloadExercises(workout)}
-//     console.log(day)
-// }
 
 //Why work out header and inner elements
 const whyWorkOutList = document.getElementsByTagName('ul')[0]
@@ -237,14 +228,24 @@ function displayListOfExercises(bodypart){
             ex.style.color = "blue"
             ex.textContent = piece.name
             exerciseList.append(ex)
-            describeLift(piece)
+            let lift = document.getElementById(piece.name)
+            lift.addEventListener('click', ()=>liftDescription(piece))
             }
         })
     .catch((error)=>alert("There is an error"))
-}
-function describeLift(lift){
-    thisLift = document.getElementById("lift")
-    thisLift.addEventListener('click', ()=> console.log(lift.muscles))
+    }
+
+function liftDescription(lift){
+    let lifted = document.getElementById(lift.name)
+    lifted.innerHTML = ""
+    let ul = document.createElement('ul')
+    ul.textContent = lift.name
+    let description = document.createElement('p')
+    description.textContent = `${lift.name} focuses on these muscles: ${lift.muscles}.`
+    let image = document.createElement('img')
+    image.src = lift.image
+    ul.append(description, image)
+    lifted.append(ul)
 }
 
 //Event listener for creating a workout
